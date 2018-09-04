2cf46b794a8a626cbe137cb3ac4b9694a3d603f5
Mikkel Ridley <mikkel250@gmail.com>
2018-09-04 21:29:03 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,10 @@
 #write your code here
 
-def countdown
+def countdown(number)
+  count = 10
+  while count < number
+    puts "#{number} SECOND(S)!"
+    count -= 1
+  end
+  return "HAPPY NEW YEAR!"
 end
